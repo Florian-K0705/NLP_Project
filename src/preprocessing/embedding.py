09ebcb9c3bf3f4a5068143410586ccdb7466626f
Florian <florian.53225@web.de>
2025-05-23 09:22:31 +0200

    Embedding umstrukturiert, da nun effizienter

--- src/preprocessing/embedding.py
+++ src/preprocessing/embedding.py
@@ -5,13 +5,19 @@ from preprocessing.tokenization import tokenize_text
 
 
 
+class Embedding:
 
-def glove_embedding(sentence):
+    def __init__(self):
+        
+        self.glove_wiki_gigaword_50_model = gensim.downloader.load('glove-wiki-gigaword-50')
 
-    tokens = tokenize_text(sentence)
 
-    embed_model = gensim.downloader.load('glove-wiki-gigaword-50')
-    emb = embed_model[tokens]
 
-    return torch.Tensor(emb)
+    def glove_embedding(self, sentence):
+
+        tokens = tokenize_text(sentence)
+
+        emb = self.glove_wiki_gigaword_50_model[tokens]
+
+        return torch.Tensor(emb)
 
